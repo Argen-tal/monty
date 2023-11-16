503f0e683be0fd09af9c8b1c2d3e68e28a262073
Damilola Olaiya <123437493+Hirosus@users.noreply.github.com>
2023-11-16 22:35:59 +0100

    Create monty.h

---
+++ monty.h
@@ -0,0 +1,56 @@
+#ifndef #MONTY_H
+#define #MONTY_H
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+* struct stack_s - doubly linked list of a stack
+* @n: the integer
+* @prev: points to the previous element of the stack
+* @next: points to the next element of the stack
+*/
+
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
+
+/**
+* struct instruction_s - opcode and its function
+* @opcode: the opcode
+* @f: function to handle the opcode
+*/
+
+typedef struct instruction_s
+{
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern int global_arg;
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void exec(char *line, unsigned int line_number stack_t **rack);
+int check atoi(char* str);
+stack_t *add_dnodeint(stack_t **head, const int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+int delete_at_index(stack_t **head, unsigned int index);
+size_t list_len(const stack_t *h);
+stack_t *insert_at_index(stack_t **h, unsigned int idx, int n);
+size_t print_list(const stack_t *h);
+char *rm_newline(char *nline, ssize_t nread);
+#endif
